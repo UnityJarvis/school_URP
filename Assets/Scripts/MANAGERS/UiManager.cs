ff5cf5e7128c43b366c564cbc5c26da2dce63970
UnityJarvis <125017786+UnityJarvis@users.noreply.github.com>
2023-05-26 18:41:33 +0530

    Minor Changes

--- Assets/Scripts/MANAGERS/UiManager.cs
+++ Assets/Scripts/MANAGERS/UiManager.cs
@@ -22,6 +22,6 @@ public class UiManager : MonoBehaviour
     {
         screenBoolPos = CanvasSwitcher.togglerer;
         CanvasSwitcher.ScreenTransition(boardInitPos, bigBoardScale, smallBoardScale, leftHandPos, worldCanvasHolderPos, canvasUIParent, screenBoolPos);
-    }
+    } 
 
 }
