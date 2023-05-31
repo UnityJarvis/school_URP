7836be7ba0b8dcd72fb8b01be29d66364135fe99
UnityJarvis <125017786+UnityJarvis@users.noreply.github.com>
2023-05-31 18:36:21 +0530

    1. Inside Room Lights and UI changes 2. Classroom Floor and wall Appearance lookout

--- Assets/Scripts/Room Construction/InsideRoomFunctionalityController.cs
+++ Assets/Scripts/Room Construction/InsideRoomFunctionalityController.cs
@@ -5,14 +5,32 @@ using UnityEngine.UI;
 public class InsideRoomFunctionalityController : MonoBehaviour
 {
     public Material roomMirrorMaterial;
-    public Slider intensitySlider;
+    public Slider propsSlider;
+    public Light propsRoomLight;
+
+    [Space(10)]
+    public Slider playerSlider;
+    public Light playerRoomLight;
+    public Material torchFrontGlass;
+
+    private void Awake()
+    {
+        roomMirrorMaterial.color = new Color(roomMirrorMaterial.color.r, roomMirrorMaterial.color.g, roomMirrorMaterial.color.b, 0f);
+    }
+
     public void Deport(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
     }
     public void lightIntensity()
     {
-        roomMirrorMaterial.color = new Color(roomMirrorMaterial.color.r, roomMirrorMaterial.color.g, roomMirrorMaterial.color.b, intensitySlider.value);
+        roomMirrorMaterial.color = new Color(roomMirrorMaterial.color.r, roomMirrorMaterial.color.g, roomMirrorMaterial.color.b, propsSlider.value);
+        propsRoomLight.intensity = (1f - propsSlider.value) * 10;
+    }
+    public void playerLightIntensity()
+    {
+        playerRoomLight.intensity = playerSlider.value;
+        torchFrontGlass.SetColor("_EmissionColor", new Color(playerSlider.value * 255, playerSlider.value * 255, playerSlider.value * 255));
     }
 }
 
