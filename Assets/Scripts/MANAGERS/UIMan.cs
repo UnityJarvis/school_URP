453ce4d5df51cb4c4810440337d80a5c452ebc2b
UnityJarvis <125017786+UnityJarvis@users.noreply.github.com>
2023-06-14 17:45:34 +0530

    Minor Changes (Haptics Backup)

--- Assets/Scripts/MANAGERS/UIMan.cs
+++ Assets/Scripts/MANAGERS/UIMan.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.Video;
 using InuCom.SchoolVR.UI.Videos;
+using Inucom.SchoolVR.UI;
 
 public class UIMan : MonoBehaviour
 {
@@ -10,8 +11,7 @@ public class UIMan : MonoBehaviour
     public List<GameObject> allPanels;
     public List<Button> startPanelButtons;
     public List<Button> experimentPanelButtons;
-    [Space(5)]
-    public Button toggleBtn;
+
 
     [Space(10)]
     [Header("VideoPanel")]
@@ -20,25 +20,40 @@ public class UIMan : MonoBehaviour
     public VideoPlayer videoPlayerComponent;
     public VideoLessons[] VideoLessonsArray;
 
+    [Space(5)]
+    [Header("Board Position Toggle")]
+    public Button toggleBtn;
+    public Vector3 boardInitPos;
+    public Vector3 bigBoardScale;
+    public Vector3 smallBoardScale;
+    public Transform leftHandPos, worldCanvasHolderPos, canvasUIParent;
+    bool screenBoolPos;
+
+    //----------------------------------------------------------------------------
+
     private void Start()
     {
         allPanels[0].GetComponentsInChildren(startPanelButtons);
         foreach (Button btn in startPanelButtons) { btn.onClick.AddListener(() => StartPanelButtonFunction(btn.name)); }
         allPanels[1].GetComponentsInChildren(experimentPanelButtons);
-        foreach (Button btn in experimentPanelButtons) { btn.onClick.AddListener(() => ExperimentPanelButtonFunction(int.Parse( btn.name))); }
+        foreach (Button btn in experimentPanelButtons) { btn.onClick.AddListener(() => ExperimentPanelButtonFunction(int.Parse(btn.name))); }
 
         viewport.GetComponentsInChildren(videoPlayerContextPanel);
 
-
-        for (int i = 0; i < videoPlayerContextPanel.Count; i++)
-        {
-            VideoPanelButtonsInstantiator.InstantiateButton(VideoLessonsArray[i], videoPlayerContextPanel[i].transform, videoPlayerComponent);
-        }
+        for (int i = 0; i < videoPlayerContextPanel.Count; i++) { VideoPanelButtonsInstantiator.InstantiateButton(VideoLessonsArray[i], videoPlayerContextPanel[i].transform, videoPlayerComponent); }
 
         //---------Disable All panels and Enable only Start Panel--------------------
         foreach (GameObject panel in allPanels) { panel.SetActive(false); }
         allPanels[0].SetActive(true);
+
+        CanvasSwitcher.togglerer = true;                //Set Position of board to wall
+        toggleBtn.onClick.AddListener(CanvasSwitcher.ScreenPosToggle);
         //---------------------------------------------------------------------------
+    }
+    private void Update()
+    {
+        screenBoolPos = CanvasSwitcher.togglerer;
+        CanvasSwitcher.ScreenTransition(boardInitPos, bigBoardScale, smallBoardScale, leftHandPos, worldCanvasHolderPos, canvasUIParent, screenBoolPos);
     }
     void StartPanelButtonFunction(string buttonName)
     {
